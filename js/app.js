4dc6a3b03a3bcdb9c190e23a584290501b0218c5
ElliotXLeo <elliotgaramendi@gmail.com>
2021-04-27 11:45:29 -0500

    Ingresos y egresos: Lista, íconos, JS y eliminado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const ingresos = [
   new Ingreso('Salario', 2000.00),
-  new Ingreso('Consultoría', 300.00)
+  new Ingreso('Consultoría', 300.00),
+  new Ingreso('Interés', 60.00)
 ];
 
 const egresos = [
@@ -8,7 +9,7 @@ const egresos = [
   new Egreso('AFP-Administración', 4.60),
   new Egreso('AFP-Seguro', 34.80),
   new Egreso('Alimentación', 250.00),
-  new Egreso('Alimentación', 1500.00)
+  new Egreso('Departamento', 1500.00)
 ];
 
 let totalIngresos = () => {
@@ -27,8 +28,12 @@ let totalEgresos = () => {
   return totalEgreso;
 }
 
-let cargarApp = () => {
-  cargarCabecero();
+const formatoMonedaSoles = (valor) => {
+  return valor.toLocaleString('es-PE', { style: 'currency', currency: 'PEN', minimumFractionDigits: 2 });
+}
+
+const formatoPorcentaje = (valor) => {
+  return valor.toLocaleString('en-US', { style: 'percent', minimumFractionDigits: 2 })
 }
 
 let cargarCabecero = () => {
@@ -42,10 +47,83 @@ let cargarCabecero = () => {
   document.getElementById('totalEgreso').innerHTML = formatoMonedaSoles(totalEgreso);
 }
 
-const formatoMonedaSoles = (valor) => {
-  return valor.toLocaleString('es-PE', { style: 'currency', currency: 'PEN', minimumFractionDigits: 2 });
+const cargarIngresos = () => {
+  let ingresosHtml = '';
+
+  for (let ingreso of ingresos) {
+    ingresosHtml += crearIngresoHtml(ingreso);
+  }
+
+  document.getElementById('listaIngresos').innerHTML = ingresosHtml;
 }
 
-const formatoPorcentaje = (valor) => {
-  return valor.toLocaleString('en-US', {style: 'percent', minimumFractionDigits: 2 })
+const crearIngresoHtml = (ingreso) => {
+  return `
+    <div class="elemento limpiarEstilos">
+      <div class="elemento_descripcion">
+        ${ingreso.descripcion}
+      </div>
+      <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">
+          + ${formatoMonedaSoles(ingreso.valor)}
+        </div>
+        <div class="elemento_eliminar">
+          <button class="elemento_eliminar--boton">
+            <ion-icon name="close-circle-outline" onclick='eliminarIngreso(${ingreso.id})'></ion-icon>
+          </button>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+const cargarEgresos = () => {
+  let egresosHtml = '';
+
+  for (let egreso of egresos) {
+    egresosHtml += crearEgresoHtml(egreso);
+  }
+
+  document.getElementById('listaEgresos').innerHTML = egresosHtml;
+}
+
+const crearEgresoHtml = (egreso) => {
+  return `
+    <div class="elemento limpiarEstilos">
+      <div class="elemento_descripcion">
+        ${egreso.descripcion}
+      </div>
+      <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">
+          - ${formatoMonedaSoles(egreso.valor)}
+        </div>
+        <div class="elemento_porcentaje">
+          ${formatoPorcentaje(egreso.valor / totalIngresos())}
+        </div>
+        <div class="elemento_eliminar">
+          <button class="elemento_eliminar--boton">
+          <ion-icon name="close-circle-outline" onclick='eliminarEgreso(${egreso.id})'></ion-icon>
+          </button>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+let cargarApp = () => {
+  cargarCabecero();
+  cargarIngresos();
+  cargarEgresos();
+}
+
+const eliminarIngreso = (id) => {
+  let indiceEliminar = ingresos.findIndex(ingreso => ingreso.id === id);
+  ingresos.splice(indiceEliminar, 1);
+  cargarApp();
+}
+
+const eliminarEgreso = (id) => {
+  let indiceEliminar = egresos.findIndex(egreso => egreso.id === id);
+  egresos.splice(indiceEliminar, 1);
+  cargarApp();
 }
